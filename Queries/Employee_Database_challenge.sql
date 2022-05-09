69b0397df65bb3ac5d3e38f75d5d1a88a9a9802c
Derrick Nyagesuka <derrickosugo34@gmail.com>
2022-05-08 21:24:49 -0500

    Updating Employee_Database_challenge.sql file.

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -13,6 +13,9 @@ ORDER BY e.emp_no;
       
 SELECT * from retirement_titles
 
+
+
+
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (emp_no) emp_no,
 first_name,
@@ -24,6 +27,10 @@ ORDER BY emp_no, title DESC;
 
 SELECT * FROM unique_titles
 
+
+
+
+
 -- Get number of employees about to retire with recent job title
 SELECT COUNT(ut.emp_no),
 ut.title
@@ -34,7 +41,11 @@ ORDER BY COUNT(title) DESC;
 
 SELECT * FROM retiring_titles
 
--- Write a query to create a Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program.
+
+
+
+
+--Create a mentorship_eligibilty.csv table that holds the employees eligible to participate in a mentorship program.
 SELECT DISTINCT ON(e.emp_no) e.emp_no, 
     e.first_name, 
     e.last_name, 
@@ -44,9 +55,11 @@ SELECT DISTINCT ON(e.emp_no) e.emp_no,
     t.title
 INTO mentorship_eligibilty
 FROM employees as e
-Left outer Join dept_emp as de
+LEFT OUTER JOIN dept_emp as de
 ON (e.emp_no = de.emp_no)
-Left outer Join titles as t
+LEFT OUTER JOIN titles as t
 ON (e.emp_no = t.emp_no)
-WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+WHERE (e.birth_date BETWEEN '1963-01-01' AND '1965-12-31')
 ORDER BY e.emp_no;
+SELECT * FROM mentorship_eligibilty
+
